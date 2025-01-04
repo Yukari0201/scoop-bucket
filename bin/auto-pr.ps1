3a8304a9481108e5e0c21b052a985f750f1d0ecc
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2025-01-04 12:22:53 +0800

    初始化；添加 mpcvr (MpcVideoRender)

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Yukari0201/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
